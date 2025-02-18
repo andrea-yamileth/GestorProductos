d3569da1ab52fb877b85cb611d17f65e930f458a
andrea-yamileth <andreachaquinga2005@gmail.com>
2025-02-17 19:15:55 -0600

    primer commit

---
+++ src/main/java/espoch/edu/ec/gestorproductoscorreccion/GestorProductosCorreccion.java
@@ -0,0 +1,10 @@
+
+package espoch.edu.ec.gestorproductoscorreccion;
+
+
+public class GestorProductosCorreccion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
